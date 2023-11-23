5c66640bab7d0e8c0d2d9ab22d1d807ec3ab6595
growgrounded <151461966+growgrounded@users.noreply.github.com>
2023-11-22 19:49:59 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+async function loadDailyDeals() {
+    try {
+        // Replace with the actual API endpoint or data source
+        const response = await fetch('https://api.example.com/deals');
+        const deals = await response.json();
+        const dealsContainer = document.getElementById('daily-deals');
+
+        deals.forEach(deal => {
+            const dealDiv = document.createElement('div');
+            dealDiv.className = 'deal';
+            dealDiv.innerHTML = `
+                <img src="${deal.imageUrl}" alt="${deal.title} Image">
+                <h3><a href="${deal.link}">${deal.title}</a></h3>
+                <p>${deal.description}</p>
+            `;
+            dealsContainer.appendChild(dealDiv);
+        });
+    } catch (error) {
+        console.error('Error loading deals:', error);
+    }
+}
+
+
+// Function to handle newsletter subscription
+function handleNewsletterSubscription(event) {
+    event.preventDefault();
+    const email = document.querySelector('#newsletter input[type="email"]').value;
+    console.log('Subscribing email:', email);
+    // Add logic to send subscription request
+}
+
+// Function to display the current date and time
+function displayDateTime() {
+    const now = new Date();
+    const dateTimeString = now.toLocaleString();
+    document.querySelector('footer').innerText = `© 2023 Farm and Off-Grid Deals - Current Time: ${dateTimeString}`;
+    setTimeout(displayDateTime, 1000);
+}
+
+// Function to initialize the website
+function init() {
+    loadDailyDeals();
+    document.querySelector('#newsletter form').addEventListener('submit', handleNewsletterSubscription);
+    displayDateTime();
+    // Add more initialization code if needed
+}
+
+// Run the init function when the window loads
+window.onload = init;
+
